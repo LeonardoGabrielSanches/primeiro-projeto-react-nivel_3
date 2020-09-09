3648f922c7133641dc76da6bcaef1e0c73013017
Leonardo Sanches <sleonardogabriel@gmail.com>
2020-09-09 00:18:13 -0300

    Add initial structure

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Gitub Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  );
+};
+
+export default Dashboard;
